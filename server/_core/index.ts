1837f35a9e51d3f3c327de4fcaa7d7a3af5599e2
johnadquin <johnadquin@gmail.com>
2026-04-10 12:27:48 +0200

    Fix: Añadida extension .js en imports locales para Vercel

--- server/_core/index.ts
+++ server/_core/index.ts
@@ -3,10 +3,12 @@ import express from "express";
 import { createServer } from "http";
 import net from "net";
 import { createExpressMiddleware } from "@trpc/server/adapters/express";
-import { registerOAuthRoutes } from "./oauth";
-import { appRouter } from "../routers";
-import { createContext } from "./context";
-import { serveStatic, setupVite } from "./vite";
+
+// AÑADIDO .js A TODAS LAS IMPORTACIONES LOCALES PARA EVITAR ERR_MODULE_NOT_FOUND
+import { registerOAuthRoutes } from "./oauth.js";
+import { appRouter } from "../routers.js";
+import { createContext } from "./context.js";
+import { serveStatic, setupVite } from "./vite.js";
 
 function isPortAvailable(port: number): Promise<boolean> {
   return new Promise(resolve => {
